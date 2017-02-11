7499ebe07d1fe22330c6127d51484cad761f6201
Chun How Tan <chunhowt@users.noreply.github.com>
2017-02-11 22:17:16 +0800

    Reads data from other directories generated through simulation. Adds dropout layer. Adds preprocessing to flip the image. Add a flag to specify the output model name.

--- drive.py
+++ drive.py
@@ -34,7 +34,7 @@ def telemetry(sid, data):
         # The current image from the center camera of the car
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)[80:, :, :]
+        image_array = np.asarray(image)[50:-30, :, :]
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
         min_speed = 8
         max_speed = 10
--- model.py
+++ model.py
@@ -1,10 +1,13 @@
-import numpy as np
-import os
+import argparse
 import csv
-import cv2
+import os
 import random
 
+import numpy as np
+import cv2
+
 from keras.models import Sequential
+from keras.layers import Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.core import Activation, Dense, Dropout, Flatten, Lambda
 from keras.layers.pooling import MaxPooling2D
@@ -13,25 +16,50 @@ import sklearn
 from sklearn.model_selection import train_test_split
 
 class Sample(object):
-  def __init__(self, image, steering):
+  def __init__(self, image, steering, flip):
     self.image_ = image
     self.steering_ = steering
+    self.flip_ = flip
 
-data_directory = 'data/'
+parser = argparse.ArgumentParser(description='Model training')
+parser.add_argument(
+    'model',
+    type=str,
+    help='Path to store model h5 file. Model should be on the same path.'
+)
+args = parser.parse_args()
 
 # First, read the sample images for training.
 samples = []
-with open(data_directory + 'driving_log.csv') as csvfile:
-  reader = csv.reader(csvfile)
-  for line in reader:
-    # Skip header line
-    if line[0] == 'center':
-      continue
-    center_image = line[0].split('/')[-1]
-    samples.append(Sample(
-      os.path.join(data_directory, 'IMG', center_image),
-      float(line[3])
-    ))
+def ReadSamples(directory):
+  results = []
+  with open(os.path.join(directory, 'driving_log.csv')) as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+      # Skip header line
+      if line[0] == 'center':
+        continue
+      center_image = line[0].split('/')[-1]
+      results.append(Sample(
+        os.path.join(directory, 'IMG', center_image),
+        float(line[3]),
+        flip=False,
+      ))
+      # Generate another mirror image by flipping the image
+      # and steering angle so that the generator can sample from
+      # both images appropriately.
+      results.append(Sample(
+        os.path.join(directory, 'IMG', center_image),
+        float(line[3]),
+        flip=True
+      ))
+  return results
+
+samples = []
+samples.extend(ReadSamples('data'))
+samples.extend(ReadSamples('recover_data'))
+samples.extend(ReadSamples('track2_data'))
+
 print('Total # samples: ', len(samples))
 
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
@@ -52,11 +80,14 @@ def generator(samples, batch_size=32):
       images = []
       angles = []
       for batch_sample in batch_samples:
-        images.append(cv2.imread(batch_sample.image_))
-        angles.append(batch_sample.steering_)
-
+        if not batch_sample.flip_:
+          images.append(cv2.imread(batch_sample.image_))
+          angles.append(batch_sample.steering_)
+        else:
+          images.append(np.fliplr(cv2.imread(batch_sample.image_)))
+          angles.append(-batch_sample.steering_)
       # trim image to only see section with road.
-      X_train = np.array(images)[:, 80:, :, :]
+      X_train = np.array(images)[:, 50:-30, :, :]
       y_train = np.array(angles)
       yield sklearn.utils.shuffle(X_train, y_train)
 
@@ -66,16 +97,22 @@ validation_generator = generator(validation_samples, batch_size=32)
 ch, row, col = 3, 80, 320
 
 model = Sequential()
+
 # Normalize the image to be between (-1, 1).
 model.add(Lambda(lambda x: (x - 127.5) / 127.5, input_shape=(row, col, ch)))
 
-# Model based on LeNet architecture.
-model.add(Convolution2D(nb_filter=6, nb_row=5, nb_col=5, subsample=(1, 1)))
+# Crops image to remove sky and bottom ground.
+# model.add(Cropping2D(cropping=((50, 0), (0, 0))))
+
+# Model based on LeNet architecture with some modification.
+model.add(Convolution2D(nb_filter=6, nb_row=5, nb_col=5, subsample=(1, 2)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
 model.add(Convolution2D(nb_filter=16, nb_row=5, nb_col=5, subsample=(1, 1)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(120))
 model.add(Activation('relu'))
@@ -91,6 +128,6 @@ model.fit_generator(
   samples_per_epoch=len(train_samples),
   validation_data=validation_generator,
   nb_val_samples=len(validation_samples),
-  nb_epoch=3)
+  nb_epoch=5)
 
-model.save('model.h5')
+model.save(args.model)
